38be243497921c99472db6eba86f233a3f795b2c
John Hartert <j.blainehartert@gmail.com>
2017-08-16 12:29:45 -0400

    Adds working server for electron

--- routes/electron.js
+++ routes/electron.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var electron = require('electron').globalPaths.push(searchPath);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'bobobobobobob' });
 });
 
 
